7de2006ba134abeb7a23a7d899fc25e3785b70c2
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-04-17 13:21:16 -0600

    feat: api con express

--- build-api/index.js
+++ build-api/index.js
@@ -1,13 +1,36 @@
 const express = require('express')
-const z = require('zod')
-const validateMovie = require('./schemas/movie.js')
+const cors = require('cors')
+const { validateMovie, validatePartialMovie } = require('./schemas/movie.js')
 const movies = require('./mocks/movies.json')
 
 const app = express()
 const PORT = process.env.PORT ?? 1234
 
-app.disable('x-powered-by')
+const ACCEPTED_ORIGINS = [
+  'http://127.0.0.1:3000',
+  'http://locahost:1234',
+  'http://127.0.0.1:5500',
+  'https://movies.com',
+]
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (ACCEPTED_ORIGINS.includes(origin))
+      callback(null, true)
+
+    if (!origin)
+      callback(error, ACCEPTED_ORIGINS)
+
+    return callback(new Error('Not allowed by CORS'))
+  },
+  optionsSuccessStatus: 200
+}
+
+app.disable('x-powered-by') // ocultamos el encabezado X-Powered-By que por defecto revela la tecnología utilizada en el servidor
+// se limita la cantidad de información que se expone sobre la infraestructura del servidor.
+app.use(cors(corsOptions)) // esto no esta del todo bien, ya que esta asignando a la cabecera Access-Control-Allow-Origin con el valor de *
 app.use(express.json()) // middleware para poder usar la data del body cuando se usan los verbos PUT, PATCH, DELETE
+
 app.get('/', (req, res) => {
   res.send('hello world')
 })
@@ -55,6 +78,38 @@ app.post('/movies', (req, res) => {
   movie.push(newMovie)
 
   res.status(201).json(newMovie) // actualizar la cache del cliente
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+
+  const movieIdx = movies.findIndex(movie => movie.id === id)
+
+  if (movieIdx === -1) return res.status(404).json({ message: 'Movie not found' })
+
+  movies.splice(movieIdx, 1)
+
+  return res.status(200).json({ message: 'Movie deleted' })
+})
+
+// ACTUALIZAMOS SOLO UNA PARTE DE LA PELICULA
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) return res.status(400).json({ error: JSON.parse(result.error.message) })
+
+  const { id } = req.params
+  const movieIdx = movies.findIndex(movie => movie.id === id)
+
+  if (movieIdx === -1) return res.status(404).json({ message: 'Movie not found' })
+
+  let movie = movies[movieIdx]
+  const updatedMovie = {
+    ...movie,
+    ...result.data
+  }
+  movie = updatedMovie
+  return res.json(updatedMovie)
 })
 
 app.use((req, res) => {

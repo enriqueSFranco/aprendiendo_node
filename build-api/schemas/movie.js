edc510a99bc6ff59956ad34549fe46bd71a8bf50
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-04-16 19:53:25 -0600

    feat(shemas): add movie schema

---
+++ build-api/schemas/movie.js
@@ -0,0 +1,24 @@
+const z = require('zod')
+
+const MovieSchema = z.object({
+  id: z.string({ invalid_type_error: 'Movie title must a string', required_error: 'Movie title is required' }),
+  title: z.string({ invalid_type_error: '' }),
+  year: z.number().int().min(1900).max(2025),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  poster: z.string().url({ message: 'Poster must be a valid URL' }).endsWith('.jpg'),
+  genre: z.array(
+    z.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
+    {
+      invalid_type_error: 'Movie genre is required.',
+      required_error: 'Movie genre must be an array of enum Genre'
+    }
+  ),
+  rate: z.number().positive().min(0).max(10).default(0)
+})
+
+function validateMovie (object) {
+  return MovieSchema.safeParse(object)
+}
+
+exports.module = { validateMovie }

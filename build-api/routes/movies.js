26e64afeffa9d9c54b2178f01d6582497b677776
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-04-19 10:56:06 -0600

    feat: router

--- build-api/routes/movies.js
+++ build-api/routes/movies.js
@@ -1,17 +1,18 @@
 import { Router } from "express"; // <- crea un enrutador para la app
-// import { loadFile } from "../utils/load-file";
 import { MovieController } from '../controllers/movies.js';
 
 const router = Router()
-// filter = {year, genre, rate}
-// const movies = loadFile('../mocks/movies.json')
-router.get('/', MovieController.getAll)
-router.get('/:id', MovieController.getById)
 
-router.post('/', MovieController.create)
-router.delete('/:id', MovieController.delete)
+export function createMovieRouter ({ model }) {
+  const movieController = new MovieController({ movieModel: model })
 
-// ACTUALIZAMOS SOLO UNA PARTE DE LA PELICULA
-router.patch('/:id', MovieController.update)
+  router.get('/', movieController.getAll)
+  router.get('/:id', movieController.getById)
 
-export default router
+  router.post('/', movieController.create)
+  router.delete('/:id', movieController.delete)
+
+  // ACTUALIZAMOS SOLO UNA PARTE DE LA PELICULA
+  router.patch('/:id', movieController.update)
+
+}

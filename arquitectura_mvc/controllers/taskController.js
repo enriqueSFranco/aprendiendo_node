de1d34e0dfb0d072fc1cff3b5f9e33f1fd0fd143
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-07-23 17:51:40 -0600

    feat(): ejercicio con MVC

--- arquitectura_mvc/controllers/taskController.js
+++ arquitectura_mvc/controllers/taskController.js
@@ -1,9 +1,12 @@
-const db = [
+let db = [
   { id: 1, title: 'Hacer ejercicio', completed: false }
 ]
 
 const addNewTask = (req, res) => {
   const { title } = req.body
+  if (!title) {
+    return res.status(400).json({ message: 'El título de la tarea es obligatorio.' })
+  }
 
   const newTask = {
     id: db.length + 1,
@@ -11,19 +14,58 @@ const addNewTask = (req, res) => {
     completed: false
   }
   db.push(newTask)
-  res.redirect('/')
+  res.status(201).json({ message: 'Tarea añadida correctamente', task: newTask })
 }
 
-const deleteTask = (req, res) => { }
+const deleteTask = (req, res) => {
+  const id = parseInt(req.params.id)
+  const taskToDelete = db.find(task => task.id === id)
 
-const editTask = (req, res) => { }
+  if (!taskToDelete)
+    return res.status(404).json({ message: `La tarea ${db[id].title} no se encontro en tu registro de tareas.` })
 
-const completeTask = (req, res) => { }
+  db = db.filter(task => task.id != id)
 
-const uncompleteTask = (req, res) => { }
+  return res.json({ message: `Tarea con ID ${id} ha sido eliminada correctamente.` })
+}
+
+const editTask = (req, res) => {
+  const id = parseInt(req.params.id)
+  const { title } = req.body
+  const taskToUpdate = db.find(task => task.id === id)
+  // console.log({ taskToUpdate, id })
+
+  if (!taskToUpdate) {
+    return res.status(404).json({ message: `La tarea ${db[id].title} no se encontro en tu registro de tareas.` })
+  }
+  taskToUpdate.title = title
+  return res.json({ message: `Tarea con ID ${id} actualizada correctamente.` })
+}
+
+const completeTask = (req, res) => {
+  const id = parseInt(req.body.id)
+  const task = db.find(task => task.id === id)
+
+  if (!task)
+    return res.status(404).json({ message: `La tarea ${db[id].title} no se encontro en tu registro de tareas.` })
+
+  task.completed = true
+  return res.json({ message: `Tarea con ID ${id} se ha completado.` })
+}
+
+const uncompleteTask = (req, res) => {
+  const id = parseInt(req.body.id)
+  const task = db.find(task => task.id === id)
+
+  if (!task)
+    return res.status(404).json({ message: `La tarea ${db[id].title} no se encontro en tu registro de tareas.` })
+
+  task.completed = false
+  return res.json({ message: `Tarea con ID ${id} se ha completado.` })
+}
 
 const getAllTasks = (req, res) => {
-  res.json(db)
+  return res.json(db)
 }
 
 const getAddTaskForm = (req, res) => { }
